5edd8e3918109e4ee73213a659c6e264c7cf2088
motaharerahnama <motaharerahnama44@gmail.com>
2025-02-04 14:18:03 +0330

    Update urls.py

--- task1/urls.py
+++ task1/urls.py
@@ -14,12 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# task1/urls.py
-
-# task1/urls.py
-
-# task1/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
